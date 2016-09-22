534f4186ce7d2aeb91bbeb24b313221c27d5b585
Sam Werngren <sam.werngren@gmail.com>
2016-09-22 21:18:18 +0100

    evaluation moved to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,4 +5,14 @@ window.onload = function(){
 function main(){
   console.log("webpack app started");
   var result = document.getElementById('result');
+  var textBox = document.getElementById('text-box')
+  console.log(result);
+  result.addEventListener('click',function(){
+    console.log("button clicked!");
+    textBox.value = eval(textBox.value);
+  }.bind(this))
+  console.log("Sam is God");
   }
+
+
+  
