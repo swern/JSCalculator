432157376b7bb2b6eb3961772927732435950e7a
Sam Werngren <sam.werngren@gmail.com>
2016-09-23 10:44:35 +0100

    5 passing tests

--- client/src/specs/calculator_spec.js
+++ client/src/specs/calculator_spec.js
@@ -13,5 +13,24 @@ describe('calculator', function(){
      assert.equal( 15, calculator.result );
    });
 
+  it( "can subtract", function() {
+     calculator.answer(" 11 - 3 ");
+     assert.equal( 8, calculator.result );
+   });
+
+  it( "can multiply", function() {
+     calculator.answer(" 0.1 * 0.2 ");
+     assert.equal( 0.02, calculator.result );
+   });
+
+  it( "can multiply", function() {
+     calculator.answer(" 30 / 2 ");
+     assert.equal( 15, calculator.result );
+   });
+
+  it ("can handle syntax error", function(){
+      calculator.answer(" 20 + @ +");
+      assert.equal( '0', calculator.result);
+  });
 
 });
